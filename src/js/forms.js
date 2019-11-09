005a9d009bc86a076541d01b005a5308a67c3099
Xelzs <axel.simonet@ynov.com>
2019-11-09 12:32:51 +0100

    update material forms

--- src/js/forms.js
+++ src/js/forms.js
@@ -1,9 +1,30 @@
-function detectInput(input) {
+/**
+ * Detect attribute & state of all inputs
+ * @param {NodeListOf<Element>} inputElements
+ */
+Axentix.detectAllInputs = function(inputElements) {
+  inputElements.forEach(input => {
+    Axentix.detectInput(input);
+  });
+};
+
+/**
+ * Detect attribute & state of an input
+ * @param {Element} input
+ */
+Axentix.detectInput = function(input) {
   const isActive = input.parentElement.classList.contains('active');
   const hasContent =
     input.value.length > 0 || input.placeholder.length > 0 || document.activeElement === input;
-  updateInput(input, isActive, hasContent);
-}
+  const isDisabled = input.hasAttribute('disabled') || input.hasAttribute('readonly');
+
+  if (input.firstInit) {
+    Axentix.updateInput(input, isActive, hasContent);
+    input.firstInit = false;
+  } else {
+    isDisabled ? '' : Axentix.updateInput(input, isActive, hasContent);
+  }
+};
 
 /**
  * Update input field
@@ -11,42 +32,44 @@ function detectInput(input) {
  * @param {boolean} isActive
  * @param {boolean} hasContent
  */
-function updateInput(input, isActive, hasContent) {
+Axentix.updateInput = function(input, isActive, hasContent) {
   if (!isActive && hasContent) {
     input.parentElement.classList.add('active');
   } else if (isActive && !hasContent) {
     input.parentElement.classList.remove('active');
   }
-}
+};
 
+/**
+ * Handle listeners
+ * @param {Event} e
+ * @param {NodeListOf<Element>} inputElements
+ */
+Axentix.handleListeners = function(e, inputElements) {
+  inputElements.forEach(input => {
+    input === e.target ? Axentix.detectInput(input) : '';
+  });
+};
+
+/**
+ * Setup forms fields listeners
+ * @param {NodeListOf<Element>} inputElements
+ */
+Axentix.setupFormsListeners = function(inputElements) {
+  inputElements.forEach(input => {
+    input.addEventListener('input', Axentix.detectInput(input));
+    input.firstInit = true;
+  });
+
+  document.addEventListener('focus', e => Axentix.handleListeners(e, inputElements), true);
+  document.addEventListener('blur', e => Axentix.handleListeners(e, inputElements), true);
+};
+
+// Init
 document.addEventListener('DOMContentLoaded', () => {
   const inputElements = document.querySelectorAll('.form-material .form-field .form-control');
   if (inputElements.length > 0) {
-    inputElements.forEach(input => {
-      detectInput(input);
-      input.addEventListener('input', () => {
-        detectInput(input);
-      });
-    });
-
-    document.addEventListener(
-      'focus',
-      e => {
-        inputElements.forEach(input => {
-          input === e.target ? detectInput(input) : '';
-        });
-      },
-      true
-    );
-
-    document.addEventListener(
-      'blur',
-      e => {
-        inputElements.forEach(input => {
-          input === e.target ? detectInput(input) : '';
-        });
-      },
-      true
-    );
+    Axentix.setupFormsListeners(inputElements);
+    Axentix.detectAllInputs(inputElements);
   }
 });
