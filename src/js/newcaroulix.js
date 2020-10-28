e87cfef73d77b63db034a09cc47afcf9110aa61b
Stallos11 <vincentdelev@yahoo.fr>
2020-10-28 10:32:32 +0100

    WIP : caroulix infinite

--- src/js/newcaroulix.js
+++ src/js/newcaroulix.js
@@ -7,6 +7,14 @@
     static getDefaultOptions() {
       return {
         animationDuration: 500,
+        animationType: 'slide',
+        backToOpposite: true,
+        isInfinite: true,
+        autoplay: {
+          enabled: false,
+          interval: 4000,
+          side: 'right',
+        },
       };
     }
 
@@ -31,14 +39,26 @@
     _setup() {
       Axentix.createEvent(this.el, 'caroulix.setup');
 
+      const animationList = ['slide'];
+      animationList.includes(this.options.animationType) ? '' : (this.options.animationType = 'slide');
+
       this.activeIndex = 0;
       this.draggedPositionX = 0;
 
       this._getChildren();
       this.children[0].classList.add('active');
+
+      if (this.options.isInfinite && this.children.length > 2) {
+        this.options.isInfinite ? (this.options.backToOpposite = true) : '';
+      } else if (this.options.isInfinite) {
+        this.options.isInfinite = false;
+      }
+
       this._waitForLoad();
 
       this._handleDrag();
+
+      this.options.autoplay.enabled ? this._setAutoPlay() : '';
     }
 
     /**
@@ -61,7 +81,8 @@
       this.totalMediaToLoad = 0;
       this.loadedMediaCount = 0;
 
-      this.children.forEach((item) => {
+      this.children.forEach((item, index) => {
+        item.dataset.index = index;
         let media = item.querySelector('img, video');
 
         if (media) {
@@ -91,31 +112,75 @@
      */
     _setItemsPosition(init = false) {
       let caroulixWidth = this.el.getBoundingClientRect().width;
+      let reversedChildren = this.children.slice().reverse();
+      let leftItemCount = -1;
 
-      this.children.forEach(
-        (child, index) =>
-          (child.style.transform = `translateX(${
+      this.children.forEach((child, index) => {});
+
+      // a voir si on peut SORTIR LE ACTIVEINDEX A SET HORS DES 3 GROS IF, en tous les cas je le place au centre
+      if (this.options.isInfinite) {
+        if (this.activeIndex == 0) {
+          this.children.forEach((child, index) => {
+            if (index <= 1) {
+              child.style.transform = `translateX(${caroulixWidth * index - this.draggedPositionX}px)`;
+            }
+          });
+          reversedChildren.forEach((child, index) => {
+            if (index < this.children.length - 2) {
+              child.style.transform = `translateX(${-caroulixWidth * (index + 1) - this.draggedPositionX}px)`;
+            }
+          });
+        } else if (this.activeIndex < this.children.length - 1) {
+          this.children.forEach((child, index) => {
+            if (index == this.activeIndex + 1) {
+              child.style.transform = `translateX(${caroulixWidth - this.draggedPositionX}px)`;
+            }
+          });
+
+          this.children
+            .slice(0, this.activeIndex)
+            .reverse()
+            .forEach((child, index) => {
+              child.style.transform = `translateX(${-caroulixWidth * (index + 1) - this.draggedPositionX}px)`;
+              leftItemCount++;
+            });
+
+          this.children.slice(this.activeIndex).forEach((child, index) => {
+            if (index == 1) {
+              child.style.transform = `translateX(${caroulixWidth - this.draggedPositionX}px)`;
+              return;
+            }
+            child.style.transform = `translateX(${
+              -caroulixWidth * (leftItemCount + index) - this.draggedPositionX
+            }px)`;
+          });
+
+          this.children[this.activeIndex].style.transform = `translateX(${-this.draggedPositionX}px)`;
+          leftItemCount = 0;
+        } else if (this.activeIndex == this.children.length - 1) {
+          console.log('lafin');
+          this.children
+            .slice()
+            .reverse()
+            .forEach((child, index) => {
+              if (child.dataset.index != this.activeIndex && index != this.children.length - 1) {
+                child.style.transform = `translateX(${-caroulixWidth * index - this.draggedPositionX}px)`;
+              } else if (index != this.children.length - 1) {
+                child.style.transform = `translateX(${-this.draggedPositionX}px)`;
+              } else if (index == this.children.length - 1) {
+                child.style.transform = `translateX(${caroulixWidth - this.draggedPositionX}px)`;
+              }
+            });
+        }
+      } else {
+        this.children.forEach((child, index) => {
+          child.style.transform = `translateX(${
             caroulixWidth * index - caroulixWidth * this.activeIndex - this.draggedPositionX
-          }px)`)
-      );
-      console.log('setPos');
+          }px)`;
+        });
+      }
+
       init ? setTimeout(() => this._setTransitionDuration(this.options.animationDuration), 50) : '';
-    }
-
-    next(step = 1) {
-      if (this.activeIndex < this.children.length - 1) {
-        this.activeIndex += step;
-        this._setItemsPosition();
-        console.log('next');
-      }
-    }
-
-    prev(step = 1) {
-      if (this.activeIndex > 0) {
-        this.activeIndex -= step;
-        this._setItemsPosition();
-        console.log('prev');
-      }
     }
 
     /**
@@ -169,7 +234,7 @@
 
       this.deltaX = this.xStart - x;
       this.deltaY = this.yStart - y;
-      console.log(x, y, this.deltaX, this.deltaY);
+      // console.log(x, y, this.deltaX, this.deltaY);
       this.draggedPositionX = this.deltaX;
       this._setItemsPosition();
     }
@@ -178,11 +243,19 @@
       e.preventDefault();
       if (this.isPressed) {
         this._setTransitionDuration(this.options.animationDuration);
+        let caroulixWidth = this.el.getBoundingClientRect().width;
 
-        this.isPressed ? (this.isPressed = false) : '';
-        this.deltaX > 0 ? this.next() : this.prev();
+        this.isPressed = false;
+
+        if (this.deltaX > (caroulixWidth * 15) / 100) {
+          this.next();
+        } else if (this.deltaX < (-caroulixWidth * 15) / 100) {
+          this.prev();
+        }
+
         this.deltaX = 0;
         this.draggedPositionX = 0;
+
         this._setItemsPosition();
       }
     }
@@ -207,6 +280,18 @@
       this.el.style.transitionDuration = duration + 'ms';
     }
 
+    _setAutoPlay() {
+      this.options.autoplay.side = this.options.autoplay.side.toLowerCase();
+
+      const sideList = ['right', 'left'];
+      sideList.includes(this.options.autoplay.side) ? '' : (this.options.autoplay.side = 'right');
+
+      setInterval(() => {
+        this.options.autoplay.side === 'right' ? this.next() : this.prev();
+        this._setItemsPosition();
+      }, this.options.autoplay.interval);
+    }
+
     /**
      * Setup listeners
      */
@@ -216,6 +301,26 @@
      * Remove listeners
      */
     _removeListeners() {}
+
+    next(step = 1) {
+      if (this.activeIndex < this.children.length - 1) {
+        this.activeIndex += step;
+        this._setItemsPosition();
+      } else if (this.options.backToOpposite) {
+        this.activeIndex = 0;
+        this._setItemsPosition();
+      }
+    }
+
+    prev(step = 1) {
+      if (this.activeIndex > 0) {
+        this.activeIndex -= step;
+        this._setItemsPosition();
+      } else if (this.options.backToOpposite) {
+        this.activeIndex = this.children.length - 1;
+        this._setItemsPosition();
+      }
+    }
   }
 
   Axentix.Config.registerComponent({
