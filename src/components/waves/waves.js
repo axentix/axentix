7575dbcba516930ba418a7f539cd77274038244c
Stallos11 <vincentdelev@yahoo.fr>
2021-10-25 12:27:06 +0200

    fix(waves): better naming & clean code

--- src/components/waves/waves.js
+++ src/components/waves/waves.js
@@ -5,24 +5,25 @@ const isMobile = 'ontouchstart' in document.documentElement;
 let wavesInstance = null;
 
 const targetMap = {};
-const boxMap = {};
+const itemMap = {};
 
 
-const createItemBox = (target) => {
+const createWaveItem = (target) => {
   const id = uid();
   const el = document.createElement('div');
   const container = document.createElement('div');
   const tagName = target.tagName.toLowerCase();
 
   target.setAttribute('ax-waves-id', id);
-  container.setAttribute('ax-waves-item-box-inner', '');
+  container.classList.add('ax-waves-item-inner');
   container.setAttribute('ax-waves-id', id);
-  el.setAttribute('ax-waves-item-box', '');
+
+  el.classList.add('ax-waves-box');
   el.setAttribute('ax-waves-id', id);
 
   el.appendChild(container);
   targetMap[id] = target;
-  boxMap[id] = el;
+  itemMap[id] = el;
 
   if (['img', 'video'].includes(tagName)) target.parentNode.appendChild(el);
   else target.appendChild(el);
@@ -31,22 +32,22 @@ const createItemBox = (target) => {
 };
 
 
-const createWaves = ({id, size, x, y, container, box, target}) => {
+const createWaves = ({id, size, x, y, container, item, target}) => {
   const waves = document.createElement('span');
 
-  waves.setAttribute('ax-waves-id', uid());
-  waves.setAttribute('ax-waves-item', '');
-  waves.setAttribute('style', `height:${size}px;
-                               width:${size}px;
-                               left:${x}px;
-                               top:${y}px;`);
+  waves.setAttribute('ax-waves-id', id);
+  waves.classList.add('ax-waves-item');
+  waves.style = ('style', `height:${size}px;
+                           width:${size}px;
+                           left:${x}px;
+                           top:${y}px;`);
 
   waves.addEventListener('animationend', () => {
     container.removeChild(waves);
-    if (!container.children.length && box) {
-      box.parentNode?.removeChild(box);
+    if (!container.children.length && item) {
+      item.parentNode?.removeChild(item);
       target.removeAttribute('ax-waves-id');
-      delete boxMap[id];
+      delete itemMap[id];
       delete targetMap[id];
     }
   }, { once: true });
@@ -59,35 +60,36 @@ const getWavesParams = (clientX, clientY, id, target) => {
   const x = clientX - left;
   const y = clientY - top;
 
-  let box = boxMap[id];
+  let item = itemMap[id];
 
-  if (!box) box = createItemBox(target);
+  if (!item) item = createWaveItem(target);
 
-  id = box.getAttribute('ax-waves-id') || uid();
+  id = item.getAttribute('ax-waves-id') || uid();
 
-  const container = box.children[0];
+  const container = item.children[0];
 
   const size = (
     Math.max(left + width - clientX, clientX - left) ** 2
     + Math.max(top + height - clientY, clientY - top) ** 2
   ) ** 0.5 * 2;
 
-  return {id, size, x, y, container, box, target};
+  return {id, size, x, y, container, item, target};
 };
 
 
-const getContainerStyle = (target, itemBox) => {
+const getContainerStyle = (target, item) => {
   const {left, top, width, height} = target.getBoundingClientRect();
-  const {left: boxLeft, top: boxTop} = itemBox.getBoundingClientRect();
+  const {left: itemLeft, top: itemTop} = item.getBoundingClientRect();
   const {borderRadius, zIndex} = window.getComputedStyle(target);
   
-  return `left:${left - boxLeft}px;
-          top:${top - boxTop}px;
+  return `left:${left - itemLeft}px;
+          top:${top - itemTop}px;
           height:${height}px;
           width:${width}px;
           border-radius:${borderRadius || '0'};
           z-index:${zIndex};`;
 };
+
 
 const getTarget = (el, id) => {
   const target = targetMap[id];
@@ -100,7 +102,6 @@ const getTarget = (el, id) => {
 
 
 const handler = (e) => {
-  if (!isMobile && e.button !== 0) return;
   const el = e.target;
   const id = el.getAttribute('ax-waves-id') || '';
   const target = getTarget(el, id);
@@ -116,44 +117,29 @@ const handler = (e) => {
 
   const wavesParams = getWavesParams(clientX, clientY, id, target);
   const waves = createWaves(wavesParams);
-  const { container, box } = wavesParams;
-  const type = isMobile ? 'touchend' : 'mouseup';
+  const { container, item } = wavesParams;
 
-  target.setAttribute('ax-waves-active', '');
-  window.addEventListener(type, (event) => {
-    if (isMobile) {
-      event.preventDefault();
-      const clickEvent = new Event('click', { "bubbles": true, "cancelable": true});
-      target.dispatchEvent(clickEvent);
-    }
-    target.removeAttribute('ax-waves-active');
-  }, { once: true });
-
-  container.setAttribute('style', getContainerStyle(target, box));
-  
+  container.setAttribute('style', getContainerStyle(target, item));  
   container.appendChild(waves);
 };
 
 
 export class Waves {
-  mainColor;
-
-  static run(mainColor) {
-    return new Waves(mainColor);
+  static run() {
+    return new Waves();
   }
 
   static destroy() {
     return wavesInstance.destroy();
   }
 
-  constructor(mainColor) {
+  constructor() {
     if (wavesInstance) return wavesInstance;
 
     const docEl = document.documentElement;
     
     if ('animation' in docEl.style) {
       const eventType = isMobile ? 'touchstart' : 'mousedown';
-      this.mainColor = mainColor;
       wavesInstance = this;
 
       window.addEventListener(eventType, handler);
