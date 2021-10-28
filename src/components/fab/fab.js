bbff404d46bdda5ba9cc7ca9bdcc104889acc354
AxelS <contact@axelsimonet.fr>
2021-10-28 15:01:25 +0200

    refactor: fab

--- src/components/fab/fab.js
+++ src/components/fab/fab.js
@@ -3,23 +3,35 @@ import { registerComponent } from '../../utils/config';
 import { instances } from '../../utils/config';
 import { createEvent, getComponentOptions } from '../../utils/utilities';
 
+/** @namespace */
+const FabOptions = {
+  animationDuration: 300,
+  hover: true,
+  direction: 'top',
+  position: 'bottom-right',
+  offsetX: '1rem',
+  offsetY: '1.5rem',
+};
+
 export class Fab extends AxentixComponent {
-  static getDefaultOptions() {
-    return {
-      animationDuration: 300,
-      hover: true,
-      direction: 'top',
-      position: 'bottom-right',
-      offsetX: '1rem',
-      offsetY: '1.5rem',
-    };
-  }
+  static getDefaultOptions = () => FabOptions;
+
+  /** Private variables */
+  #isAnimated = false;
+  #isActive = false;
+  /** @type {HTMLElement} */
+  #trigger;
+  /** @type {HTMLElement} */
+  #fabMenu;
+  #openRef;
+  #closeRef;
+  #documentClickRef;
+  #listenerRef;
 
   /**
-   * Construct Fab instance
-   * @constructor
-   * @param {String} element
-   * @param {Object} options
+   * @param {string} element
+   * @param {FabOptions} [options]
+   * @param {boolean} [isLoadedWithData]
    */
   constructor(element, options, isLoadedWithData) {
     super();
@@ -30,150 +42,120 @@ export class Fab extends AxentixComponent {
 
       this.el = document.querySelector(element);
 
+      /** @type {FabOptions} */
       this.options = getComponentOptions('Fab', options, this.el, isLoadedWithData);
 
-      this._setup();
+      this.#setup();
     } catch (error) {
       console.error('[Axentix] Fab init error', error);
     }
   }
 
-  /**
-   * Setup component
-   */
-  _setup() {
+  #setup() {
     createEvent(this.el, 'fab.setup');
 
-    this.isAnimated = false;
-    this.isActive = false;
-    this.trigger = document.querySelector('#' + this.el.id + ' .fab-trigger');
-    this.fabMenu = document.querySelector('#' + this.el.id + ' .fab-menu');
+    this.#isAnimated = false;
+    this.#isActive = false;
+    this.#trigger = document.querySelector('#' + this.el.id + ' .fab-trigger');
+    this.#fabMenu = document.querySelector('#' + this.el.id + ' .fab-menu');
 
-    this._verifOptions();
-    this._setupListeners();
+    this.#verifOptions();
+    this.setupListeners();
     this.el.style.transitionDuration = this.options.animationDuration + 'ms';
-    this._setProperties();
+    this.#setProperties();
   }
 
-  /**
-   * Options check
-   */
-  _verifOptions() {
+  #verifOptions() {
     const directionList = ['right', 'left', 'top', 'bottom'];
-    directionList.includes(this.options.direction) ? '' : (this.options.direction = 'top');
+    if (!directionList.includes(this.options.direction)) this.options.direction = 'top';
 
     const positionList = ['top-right', 'top-left', 'bottom-right', 'bottom-left'];
-    positionList.includes(this.options.position) ? '' : (this.options.position = 'bottom-right');
+    if (!positionList.includes(this.options.position)) this.options.position = 'bottom-right';
   }
 
-  /**
-   * Setup listeners
-   */
-  _setupListeners() {
+  setupListeners() {
     if (this.options.hover) {
-      this.openRef = this.open.bind(this);
-      this.closeRef = this.close.bind(this);
-      this.el.addEventListener('mouseenter', this.openRef);
-      this.el.addEventListener('mouseleave', this.closeRef);
+      this.#openRef = this.open.bind(this);
+      this.#closeRef = this.close.bind(this);
+      this.el.addEventListener('mouseenter', this.#openRef);
+      this.el.addEventListener('mouseleave', this.#closeRef);
     } else {
-      this.listenerRef = this._onClickTrigger.bind(this);
-      this.el.addEventListener('click', this.listenerRef);
+      this.#listenerRef = this.#onClickTrigger.bind(this);
+      this.el.addEventListener('click', this.#listenerRef);
     }
 
-    this.documentClickRef = this._handleDocumentClick.bind(this);
-    document.addEventListener('click', this.documentClickRef, true);
+    this.#documentClickRef = this.#handleDocumentClick.bind(this);
+    document.addEventListener('click', this.#documentClickRef, true);
   }
 
-  /**
-   * Remove listeners
-   */
-  _removeListeners() {
+  removeListeners() {
     if (this.options.hover) {
-      this.el.removeEventListener('mouseenter', this.openRef);
-      this.el.removeEventListener('mouseleave', this.closeRef);
-      this.openRef = undefined;
-      this.closeRef = undefined;
+      this.el.removeEventListener('mouseenter', this.#openRef);
+      this.el.removeEventListener('mouseleave', this.#closeRef);
+      this.#openRef = undefined;
+      this.#closeRef = undefined;
     } else {
-      this.el.removeEventListener('click', this.listenerRef);
-      this.listenerRef = undefined;
+      this.el.removeEventListener('click', this.#listenerRef);
+      this.#listenerRef = undefined;
     }
 
-    document.removeEventListener('click', this.documentClickRef, true);
-    this.documentClickRef = undefined;
+    document.removeEventListener('click', this.#documentClickRef, true);
+    this.#documentClickRef = undefined;
   }
 
-  /**
-   * Set different options on element
-   */
-  _setProperties() {
-    this.options.position.split('-')[0] === 'top'
-      ? (this.el.style.top = this.options.offsetY)
-      : (this.el.style.bottom = this.options.offsetY);
+  #setProperties() {
+    if (this.options.position.split('-')[0] === 'top') this.el.style.top = this.options.offsetY;
+    else this.el.style.bottom = this.options.offsetY;
 
-    this.options.position.split('-')[1] === 'right'
-      ? (this.el.style.right = this.options.offsetX)
-      : (this.el.style.left = this.options.offsetX);
+    if (this.options.position.split('-')[1] === 'right') this.el.style.right = this.options.offsetX;
+    else this.el.style.left = this.options.offsetX;
 
-    this.options.direction === 'top' || this.options.direction === 'bottom'
-      ? ''
-      : this.el.classList.add('fab-dir-x');
+    if (this.options.direction === 'right' || this.options.direction === 'left')
+      this.el.classList.add('fab-dir-x');
 
-    this._setMenuPosition();
+    this.#setMenuPosition();
   }
 
-  /**
-   * Set menu position
-   */
-  _setMenuPosition() {
+  #setMenuPosition() {
     if (this.options.direction === 'top' || this.options.direction === 'bottom') {
-      const height = this.trigger.clientHeight;
-      this.options.direction === 'top'
-        ? (this.fabMenu.style.bottom = height + 'px')
-        : (this.fabMenu.style.top = height + 'px');
+      const height = this.#trigger.clientHeight;
+      if (this.options.direction === 'top') this.#fabMenu.style.bottom = height + 'px';
+      else this.#fabMenu.style.top = height + 'px';
     } else {
-      const width = this.trigger.clientWidth;
-      this.options.direction === 'right'
-        ? (this.fabMenu.style.left = width + 'px')
-        : (this.fabMenu.style.right = width + 'px');
+      const width = this.#trigger.clientWidth;
+      if (this.options.direction === 'right') this.#fabMenu.style.left = width + 'px';
+      else this.#fabMenu.style.right = width + 'px';
     }
   }
 
-  /**
-   * Handle document click event
-   * @param {Event} e
-   */
-  _handleDocumentClick(e) {
+  /** @param {Event} e */
+  #handleDocumentClick(e) {
     const isInside = this.el.contains(e.target);
 
-    !isInside && this.isActive ? this.close() : '';
+    if (!isInside && this.#isActive) this.close();
   }
 
-  /**
-   * Handle click on trigger
-   * @param {Event} e
-   */
-  _onClickTrigger(e) {
+  /** @param {Event} e */
+  #onClickTrigger(e) {
     e.preventDefault();
-    if (this.isAnimated) {
-      return;
-    }
+    if (this.#isAnimated) return;
 
-    this.isActive ? this.close() : this.open();
+    if (this.#isActive) this.close();
+    else this.open();
   }
 
   /**
    * Open fab
    */
   open() {
-    if (this.isActive) {
-      return;
-    }
+    if (this.#isActive) return;
+
     createEvent(this.el, 'fab.open');
-    this.isAnimated = true;
-    this.isActive = true;
+    this.#isAnimated = true;
+    this.#isActive = true;
     this.el.classList.add('active');
     setTimeout(() => {
-      this.isAnimated = false;
+      this.#isAnimated = false;
     }, this.options.animationDuration);
   }
 
@@ -181,15 +163,14 @@ export class Fab extends AxentixComponent {
    * Close fab
    */
   close() {
-    if (!this.isActive) {
-      return;
-    }
+    if (!this.#isActive) return;
+
     createEvent(this.el, 'fab.close');
-    this.isAnimated = true;
-    this.isActive = false;
+    this.#isAnimated = true;
+    this.#isActive = false;
     this.el.classList.remove('active');
     setTimeout(() => {
-      this.isAnimated = false;
+      this.#isAnimated = false;
     }, this.options.animationDuration);
   }
 }
