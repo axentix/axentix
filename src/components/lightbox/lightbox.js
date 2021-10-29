31142a6431d11b2b079a4a9df7e121d217b4eed0
AxelS <contact@axelsimonet.fr>
2021-10-29 12:21:30 +0200

    refactor: lightbox

--- src/components/lightbox/lightbox.js
+++ src/components/lightbox/lightbox.js
@@ -3,22 +3,40 @@ import { registerComponent } from '../../utils/config';
 import { instances } from '../../utils/config';
 import { createEvent, getComponentOptions, wrap } from '../../utils/utilities';
 
+/** @namespace */
+const LightboxOptions = {
+  animationDuration: 400,
+  overlayClass: 'grey dark-4',
+  offset: 150,
+  mobileOffset: 80,
+  caption: '',
+};
+
 export class Lightbox extends AxentixComponent {
-  static getDefaultOptions() {
-    return {
-      animationDuration: 400,
-      overlayClass: 'grey dark-4',
-      offset: 150,
-      mobileOffset: 80,
-      caption: '',
-    };
-  }
+  static getDefaultOptions = () => LightboxOptions;
+
+  /** Private variables */
+  #openOnClickRef;
+  #closeEventRef;
+  /** @type {HTMLElement} */
+  #overlay;
+  #overlayClickEventRef;
+  /** @type {Array<HTMLElement>} */
+  #overflowParents;
+  #basicWidth = 0;
+  #basicHeight = 0;
+  #newTop = 0;
+  #newLeft = 0;
+  #isActive = false;
+  #isAnimated = false;
+  #isResponsive = false;
+  /** @type {HTMLDivElement} */
+  #container;
 
   /**
-   * Construct Lightbox instance
-   * @constructor
-   * @param {String} element
-   * @param {Object} options
+   * @param {string} element
+   * @param {LightboxOptions} [options]
+   * @param {boolean} [isLoadedWithData]
    */
   constructor(element, options, isLoadedWithData) {
     super();
@@ -29,98 +47,89 @@ export class Lightbox extends AxentixComponent {
 
       this.el = document.querySelector(element);
 
+      /** @type {LightboxOptions} */
       this.options = getComponentOptions('Lightbox', options, this.el, isLoadedWithData);
 
-      this._setup();
+      this.#setup();
     } catch (error) {
       console.error('[Axentix] Lightbox init error', error);
     }
   }
 
-  /**
-   * Setup component
-   */
-  _setup() {
+  #setup() {
     createEvent(this.el, 'lightbox.setup');
     this.el.style.transitionDuration = this.options.animationDuration + 'ms';
 
-    this.container = wrap([this.el]);
+    this.#container = wrap([this.el]);
 
-    this._setupListeners();
+    this.setupListeners();
   }
 
-  /**
-   * Setup listeners
-   */
-  _setupListeners() {
-    this.openOnClickRef = this.open.bind(this);
-    this.el.addEventListener('click', this.openOnClickRef);
+  setupListeners() {
+    this.#openOnClickRef = this.open.bind(this);
+    this.el.addEventListener('click', this.#openOnClickRef);
 
-    this.closeEventRef = this.close.bind(this);
-    window.addEventListener('keyup', this.closeEventRef);
-    window.addEventListener('scroll', this.closeEventRef);
-    window.addEventListener('resize', this.closeEventRef);
+    this.#closeEventRef = this.close.bind(this);
+    window.addEventListener('keyup', this.#closeEventRef);
+    window.addEventListener('scroll', this.#closeEventRef);
+    window.addEventListener('resize', this.#closeEventRef);
   }
 
-  /**
-   * Remove event listeners
-   */
-  _removeListeners() {
-    this.el.removeEventListener('click', this.openOnClickRef);
+  removeListeners() {
+    this.el.removeEventListener('click', this.#openOnClickRef);
 
-    window.removeEventListener('keyup', this.closeEventRef);
-    window.removeEventListener('scroll', this.closeEventRef);
-    window.removeEventListener('resize', this.closeEventRef);
+    window.removeEventListener('keyup', this.#closeEventRef);
+    window.removeEventListener('scroll', this.#closeEventRef);
+    window.removeEventListener('resize', this.#closeEventRef);
 
-    this.openOnClickRef = undefined;
-    this.onResizeRef = undefined;
-    this.closeEventRef = undefined;
+    this.#openOnClickRef = undefined;
+    this.#closeEventRef = undefined;
   }
 
-  _setOverlay() {
-    this.overlay = document.createElement('div');
-    this.overlay.style.transitionDuration = this.options.animationDuration + 'ms';
-    this.overlay.className = 'lightbox-overlay ' + this.options.overlayClass;
-    this.container.appendChild(this.overlay);
+  #setOverlay() {
+    this.#overlay = document.createElement('div');
+    this.#overlay.style.transitionDuration = this.options.animationDuration + 'ms';
+    this.#overlay.className = 'lightbox-overlay ' + this.options.overlayClass;
+    this.#container.appendChild(this.#overlay);
 
     if (this.options.caption) {
-      this.caption = document.createElement('p');
-      this.caption.className = 'lightbox-caption';
-      this.caption.innerHTML = this.options.caption;
-      this.overlay.appendChild(this.caption);
+      const caption = document.createElement('p');
+      caption.className = 'lightbox-caption';
+      caption.innerHTML = this.options.caption;
+      this.#overlay.appendChild(caption);
     }
 
-    this.overlayClickEventRef = this.close.bind(this);
-    this.overlay.addEventListener('click', this.overlayClickEventRef);
+    this.#overlayClickEventRef = this.close.bind(this);
+    this.#overlay.addEventListener('click', this.#overlayClickEventRef);
   }
 
-  _showOverlay() {
-    this.overlay.style.opacity = 1;
+  #showOverlay() {
+    this.#overlay.style.opacity = 1;
   }
 
-  _unsetOverlay() {
-    this.overlay.style.opacity = 0;
+  #unsetOverlay() {
+    this.#overlay.style.opacity = 0;
 
-    this.overlay.removeEventListener('click', this.overlayClickEventRef);
+    this.#overlay.removeEventListener('click', this.#overlayClickEventRef);
     setTimeout(() => {
-      this.overlay.remove();
+      this.#overlay.remove();
     }, this.options.animationDuration);
   }
 
-  _calculateRatio() {
-    let offset = window.innerWidth >= 960 ? this.options.offset : this.options.mobileOffset;
+  #calculateRatio() {
+    const offset = window.innerWidth >= 960 ? this.options.offset : this.options.mobileOffset;
 
-    if (window.innerWidth / window.innerHeight >= this.basicWidth / this.basicHeight) {
+    if (window.innerWidth / window.innerHeight >= this.#basicWidth / this.#basicHeight) {
       this.newHeight = window.innerHeight - offset;
-      this.newWidth = (this.newHeight * this.basicWidth) / this.basicHeight;
+      this.newWidth = (this.newHeight * this.#basicWidth) / this.#basicHeight;
     } else {
       this.newWidth = window.innerWidth - offset;
-      this.newHeight = (this.newWidth * this.basicHeight) / this.basicWidth;
+      this.newHeight = (this.newWidth * this.#basicHeight) / this.#basicWidth;
     }
   }
 
-  _setOverflowParents() {
-    this.overflowParents = [];
+  #setOverflowParents() {
+    this.#overflowParents = [];
     for (let elem = this.el; elem && elem !== document; elem = elem.parentNode) {
       const elementSyle = window.getComputedStyle(elem);
       if (
@@ -128,32 +137,24 @@ export class Lightbox extends AxentixComponent {
         elementSyle.overflowX === 'hidden' ||
         elementSyle.overflowY === 'hidden'
       ) {
-        this.overflowParents.push(elem);
+        this.#overflowParents.push(elem);
         elem.style.setProperty('overflow', 'visible', 'important');
         document.body.style.overflowX = 'hidden';
       }
     }
   }
 
-  _unsetOverflowParents() {
-    this.overflowParents.map((parent) => (parent.style.overflow = ''));
+  #unsetOverflowParents() {
+    this.#overflowParents.map((parent) => (parent.style.overflow = ''));
     document.body.style.overflowX = '';
   }
 
-  /**
-   * Set position of active lightbox
-   */
+  /** Open lightbox */
   open() {
-    if (this.isActive) {
-      this.close();
-      return;
-    } else if (this.isAnimated) {
-      return;
-    }
+    if (this.#isActive) return this.close();
+    else if (this.#isAnimated) return;
 
-    this.overflowElements = [];
-
-    this._setOverflowParents();
+    this.#setOverflowParents();
 
     const centerTop = window.innerHeight / 2;
     const centerLeft = window.innerWidth / 2;
@@ -161,48 +162,48 @@ export class Lightbox extends AxentixComponent {
     const rect = this.el.getBoundingClientRect();
     const containerRect = this.el.getBoundingClientRect();
 
-    this.basicWidth = rect.width;
-    this.el.style.width = this.basicWidth + 'px';
-    this.basicHeight = rect.height;
-    this.el.style.height = this.basicHeight + 'px';
+    this.#basicWidth = rect.width;
+    this.el.style.width = this.#basicWidth + 'px';
+    this.#basicHeight = rect.height;
+    this.el.style.height = this.#basicHeight + 'px';
 
     this.el.style.top = 0;
     this.el.style.left = 0;
 
-    this.newTop = centerTop + window.scrollY - (containerRect.top + window.scrollY);
-    this.newLeft = centerLeft + window.scrollX - (containerRect.left + window.scrollX);
+    this.#newTop = centerTop + window.scrollY - (containerRect.top + window.scrollY);
+    this.#newLeft = centerLeft + window.scrollX - (containerRect.left + window.scrollX);
 
-    this._calculateRatio();
+    this.#calculateRatio();
 
-    this.container.style.position = 'relative';
-    this._setOverlay();
+    this.#container.style.position = 'relative';
+    this.#setOverlay();
 
     setTimeout(() => {
       createEvent(this.el, 'lightbox.open');
 
-      this.isAnimated = true;
+      this.#isAnimated = true;
 
       this.el.classList.add('active');
 
       if (this.el.classList.contains('responsive-media')) {
         this.el.classList.remove('responsive-media');
-        this.isResponsive = true;
+        this.#isResponsive = true;
       } else {
-        this.isResponsive = false;
+        this.#isResponsive = false;
       }
 
-      this.isActive = true;
+      this.#isActive = true;
 
-      this._showOverlay();
-      this.container.style.width = this.basicWidth + 'px';
-      this.container.style.height = this.basicHeight + 'px';
+      this.#showOverlay();
+      this.#container.style.width = this.#basicWidth + 'px';
+      this.#container.style.height = this.#basicHeight + 'px';
 
       this.el.style.width = this.newWidth + 'px';
       this.el.style.height = this.newHeight + 'px';
-      this.el.style.top = this.newTop - this.newHeight / 2 + 'px';
-      this.el.style.left = this.newLeft - this.newWidth / 2 + 'px';
+      this.el.style.top = this.#newTop - this.newHeight / 2 + 'px';
+      this.el.style.left = this.#newLeft - this.newWidth / 2 + 'px';
 
-      this.isAnimated = false;
+      this.#isAnimated = false;
     }, 50);
 
     setTimeout(() => {
@@ -210,43 +211,37 @@ export class Lightbox extends AxentixComponent {
     }, this.options.animationDuration + 50);
   }
 
-  /**
-   * Unset active lightbox
-   */
+  /** Close lightbox */
   close(e) {
-    if (!this.isActive || (e && e.key && e.key !== 'Escape')) {
-      return;
-    } else if (this.isAnimated) {
-      return;
-    }
+    if (!this.#isActive || (e && e.key && e.key !== 'Escape') || this.#isAnimated) return;
 
-    this.isAnimated = true;
+    this.#isAnimated = true;
 
     this.el.style.top = 0;
     this.el.style.left = 0;
 
-    this.el.style.width = this.basicWidth + 'px';
-    this.el.style.height = this.basicHeight + 'px';
-    this._unsetOverlay();
+    this.el.style.width = this.#basicWidth + 'px';
+    this.el.style.height = this.#basicHeight + 'px';
+    this.#unsetOverlay();
 
     createEvent(this.el, 'lightbox.close');
 
     setTimeout(() => {
       this.el.classList.remove('active');
 
-      this.isResponsive ? this.el.classList.add('responsive-media') : '';
+      if (this.#isResponsive) this.el.classList.add('responsive-media');
 
-      this.container.removeAttribute('style');
+      this.#container.removeAttribute('style');
       this.el.style.left = '';
       this.el.style.top = '';
       this.el.style.width = '';
       this.el.style.height = '';
       this.el.style.transform = '';
 
-      this._unsetOverflowParents();
+      this.#unsetOverflowParents();
 
-      this.isActive = false;
-      this.isAnimated = false;
+      this.#isActive = false;
+      this.#isAnimated = false;
 
       createEvent(this.el, 'lightbox.closed');
     }, this.options.animationDuration + 50);
