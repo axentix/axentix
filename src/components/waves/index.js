6084cfe1634c59eed578396e0e712445d87d2c36
Stallos11 <vincentdelev@yahoo.fr>
2021-11-17 17:31:13 +0100

    fix(waves): ts refactor

--- src/components/waves/index.js
+++
@@ -1,5 +0,0 @@
-import './waves.scss';
-
-import { waves } from './waves';
-
-export default waves;
--- src/components/waves/waves.ts
+++ src/components/waves/waves.ts
@@ -1,14 +1,24 @@
 let i = 0;
-const uid = () => ++i;
-const isMobile = 'ontouchstart' in document.documentElement;
+const uid = () => (++i).toString();
+const isMobile: Boolean = 'ontouchstart' in document.documentElement;
 const targetMap = {};
 const itemMap = {};
 
-const createWaveItem = (target) => {
-  const id = uid();
-  const el = document.createElement('div');
-  const container = document.createElement('div');
-  const tagName = target.tagName.toLowerCase();
+interface wavesParams {
+  id: string,
+  size: number,
+  x: number,
+  y: number,
+  container: HTMLElement,
+  item: HTMLElement,
+  target: HTMLElement
+}
+
+const createWaveItem = (target: HTMLElement) => {
+  const id: string = uid();
+  const el: HTMLElement = document.createElement('div');
+  const container: HTMLElement = document.createElement('div');
+  const tagName: string = target.tagName.toLowerCase();
 
   target.setAttribute('ax-waves-id', id);
   container.classList.add('ax-waves-item-inner');
@@ -27,7 +37,7 @@ const createWaveItem = (target) => {
   return el;
 };
 
-const createWaves = ({id, size, x, y, container, item, target}, color) => {
+const createWaves = ({id, size, x, y, container, item, target}: wavesParams, color: string) => {
   const waves = document.createElement('span');
 
   let style = `height:${size}px;
@@ -39,7 +49,7 @@ const createWaves = ({id, size, x, y, container, item, target}, color) => {
 
   waves.setAttribute('ax-waves-id', id);
   waves.classList.add('ax-waves-item');
-  waves.style = (style);
+  waves.setAttribute('style', style);
 
   waves.addEventListener('animationend', () => {
     container.removeChild(waves);
@@ -54,18 +64,18 @@ const createWaves = ({id, size, x, y, container, item, target}, color) => {
   return waves;
 };
 
-const getWavesParams = (clientX, clientY, id, target) => {
+const getWavesParams = (clientX: number, clientY: number, id: string, target: HTMLElement): wavesParams => {
   const {top, left, width, height} = target.getBoundingClientRect();
-  const x = clientX - left;
-  const y = clientY - top;
-  let item = itemMap[id];
+  const x: number = clientX - left;
+  const y: number = clientY - top;
+  let item: any = itemMap[id];
 
   if (!item) item = createWaveItem(target);
   id = item.getAttribute('ax-waves-id') || uid();
 
-  const container = item.children[0];
+  const container: HTMLElement = item.children[0];
 
-  const size = (
+  const size: number = (
     Math.max(left + width - clientX, clientX - left) ** 2
     + Math.max(top + height - clientY, clientY - top) ** 2
   ) ** 0.5 * 2;
@@ -74,7 +84,7 @@ const getWavesParams = (clientX, clientY, id, target) => {
 };
 
 
-const getContainerStyle = (target, item) => {
+const getContainerStyle = (target: HTMLElement, item: HTMLElement): string => {
   const {left, top, width, height} = target.getBoundingClientRect();
   const {left: itemLeft, top: itemTop} = item.getBoundingClientRect();
   const {borderRadius, zIndex} = window.getComputedStyle(target);
@@ -87,7 +97,7 @@ const getContainerStyle = (target, item) => {
           z-index:${zIndex};`;
 };
 
-const getTarget = (el, id) => {
+const getTarget = (el: HTMLElement, id: string) => {
   const target = targetMap[id];
   
   if (target) return target;
@@ -111,17 +121,17 @@ const handler = (e) => {
     clientY = click.clientY;
   }
 
-  const wavesParams = getWavesParams(clientX, clientY, id, target);
-  const waves = createWaves(wavesParams, color);
-  const { container, item } = wavesParams;
-
+  const params: wavesParams = getWavesParams(clientX, clientY, id, target);
+  const waves = createWaves(params, color);
+  const { container, item } = params;
+  
   container.setAttribute('style', getContainerStyle(target, item));
   container.appendChild(waves);
 };
 
-export const waves = () => {
+export const Waves = () => {
   const eventType = isMobile ? 'touchstart' : 'mousedown';
   window.addEventListener(eventType, handler);
 }
 
-document.addEventListener('DOMContentLoaded', waves());
+document.addEventListener('DOMContentLoaded', () => Waves());
