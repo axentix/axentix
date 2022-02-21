73a6f80c660c059c79259780f7c12d353ecddc2b
AxelS <contact@axelsimonet.fr>
2022-02-21 15:52:49 +0100

    fix: select.ts & utils build

--- scripts/build.js
+++ scripts/build.js
@@ -6,17 +6,21 @@ const { visualizer } = require('rollup-plugin-visualizer');
 
 const buildComponent = async (filepath) => {
   const fileSplitted = filepath.split('/');
-  const name = fileSplitted[fileSplitted.length - 2];
+  const name = filepath.endsWith('select.ts')
+    ? fileSplitted[fileSplitted.length - 1].replace('.ts', '')
+    : fileSplitted[fileSplitted.length - 2];
   const c = config(name[0].toUpperCase() + name.slice(1), filepath);
 
   c.build.lib.entry = resolve(__dirname, `../${filepath}`);
+  if (filepath.endsWith('select.ts'))
+    c.build.rollupOptions.output.dir = `dist/${filepath.replace('src/', '').replace('select.ts', '')}`;
 
   await build(c);
 };
 
 const buildUtil = async (filepath) => {
   const fileSplitted = filepath.split('/');
-  const name = fileSplitted[fileSplitted.length - 1];
+  const name = fileSplitted[fileSplitted.length - 1].replace('.ts', '');
   const c = config(name[0].toUpperCase() + name.slice(1), filepath);
 
   c.build.lib.entry = resolve(__dirname, `../${filepath}`);
@@ -41,7 +45,7 @@ if (process.argv[2] === 'main') {
   return;
 }
 
-glob('{**/**/index.ts,src/utils/*.ts}').then(async (files) => {
+glob('{**/**/index.ts,src/utils/*.ts,src/**/select.ts}').then(async (files) => {
   const mainPath = files.find((f) => f === 'src/index.ts' && f.split('/')[1] !== 'utils');
   await buildMain(mainPath);
 
