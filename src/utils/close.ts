551f01afae033c0b980033e072f4fcc483f44921
AxelS <contact@axelsimonet.fr>
2022-08-01 17:00:18 +0200

    fix: close utility

--- src/utils/close.ts
+++ src/utils/close.ts
@@ -2,13 +2,13 @@ const removeTarget = (e: Event) => {
   const targetId = (e.target as HTMLElement).getAttribute('data-close');
   if (!targetId) return console.error('[Axentix] A closable target must be set');
 
-  const target = document.querySelector(targetId);
+  const target = document.getElementById(targetId);
   if (target) target.remove();
 };
 
 const setup = () => {
-  const closableElements = document.querySelectorAll('[data-close]');
-  closableElements.forEach((el) => el.addEventListener('click', removeTarget));
+  const elements = document.querySelectorAll('[data-close]');
+  elements.forEach((el) => el.addEventListener('click', removeTarget));
 };
 
 document.addEventListener('DOMContentLoaded', setup);
